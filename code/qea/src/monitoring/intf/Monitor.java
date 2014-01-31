40d1aa2bc83d6cc4731790f793af2812120ed9e6
Helena <Helena@VAIO-HELENA>
2014-01-31 02:23:15 +0000

    *Interface 'Monitor' turned into an abstract class *Event representation changed from 'Event' to integer *New methods 'step' added from 1 to 5 parameters *Abstract method 'end' added *Method 'trace' deleted

--- code/qea/src/monitoring/intf/Monitor.java
+++ code/qea/src/monitoring/intf/Monitor.java
@@ -1,17 +1,42 @@
 package monitoring.intf;
 
-import java.util.List;
-
 import structure.impl.Verdict;
-import structure.intf.Event;
 
 /*
  * A monitor object takes an event consisting of a name and an array of arguments and produces a verdict
  */
-public interface Monitor {
+public abstract class Monitor {
 
-	public Verdict step(Event event);
-	
-	public Verdict trace(List<Event> eventList);
-	
+	public abstract Verdict step(int eventName, Object[] args);
+
+	public Verdict step(int eventName, Object param1) {
+		return step(eventName, new Object[] { param1 });
+	}
+
+	public Verdict step(int eventName, Object param1, Object param2) {
+		return step(eventName, new Object[] { param1, param2 });
+	}
+
+	public Verdict step(int eventName, Object param1, Object param2,
+			Object param3) {
+		return step(eventName, new Object[] { param1, param2, param3 });
+	}
+
+	public Verdict step(int eventName, Object param1, Object param2,
+			Object param3, Object param4) {
+		return step(eventName, new Object[] { param1, param2, param3, param4 });
+	}
+
+	public Verdict step(int eventName, Object param1, Object param2,
+			Object param3, Object param4, Object param5) {
+		return step(eventName, new Object[] { param1, param2, param3, param4,
+				param5 });
+	}
+
+	public abstract Verdict end();
+
+	// TODO: The trace method received before a list of Events, now we don't
+	// have 'Event'
+	// public abstract Verdict trace(List<Event> eventList);
+
 }
