f8d06158c60218c2c4d79d60133959a77b59252a
Helena <Helena@VAIO-HELENA>
2014-01-31 02:19:54 +0000

    *First implementation of method 'step' for 1 parameter added *First implementation of method 'end' added

--- code/qea/src/monitoring/impl/UnoptSimplestSmallStepQEAMonitor.java
+++ code/qea/src/monitoring/impl/UnoptSimplestSmallStepQEAMonitor.java
@@ -1,26 +1,86 @@
 package monitoring.impl;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+import structure.impl.SimplestQEA;
 import structure.impl.Verdict;
-import structure.intf.Event;
 import structure.intf.QEA;
 
 /**
  * An unoptimised small-step monitor for the Simplest QEA
  * 
- * @author Giles Reger and Helena Cuenca
+ * @author Giles Reger
+ * @author Helena Cuenca
  */
 
-public class UnoptSimplestSmallStepQEAMonitor extends SmallStepMonitor{
+public class UnoptSimplestSmallStepQEAMonitor extends SmallStepMonitor {
 
-	UnoptSimplestSmallStepQEAMonitor(QEA qea){
-		
+	private IdentityHashMap<Object, Integer> bindings;
+
+	private QEA qea;
+
+	private SimplestQEA simplestQEA;
+
+	UnoptSimplestSmallStepQEAMonitor(QEA qea) {
+		bindings = new IdentityHashMap<>();
+		this.qea = qea;
+		simplestQEA = (SimplestQEA) qea; // TODO Change this!
 	}
 
-	public Verdict step(Event event) {
+	@Override
+	public Verdict step(int eventName, Object[] args) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	public Verdict step(int eventName, Object param1) {
 
+		// Check if the data value corresponds to an existing binding
+		if (bindings.containsKey(param1)) {
+
+			int nextState = simplestQEA.getNextState(bindings.get(param1),
+					eventName);
+			if (nextState != 0) {
+				bindings.put(param1, nextState);
+			}
+		} else { // New binding
+			int nextState = simplestQEA.getNextState(1, eventName);
+			if (nextState != 0) {
+				bindings.put(param1, nextState);
+			} else {
+				// TODO Assume initial state or retrieve it from the QEA?
+				bindings.put(param1, 1);
+			}
+		}
+
+		if (allBindingsInFinalState()) {
+			return Verdict.WEAK_SUCCESS;
+		}
+		return Verdict.WEAK_FAILURE;
+	}
+
+	@Override
+	public Verdict end() {
+		if (allBindingsInFinalState()) {
+			return Verdict.SUCCESS;
+		}
+		return Verdict.FAILURE;
+	}
+
+	// public Verdict step(Event event) {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+
+	private boolean allBindingsInFinalState() {
+		for (Map.Entry<Object, Integer> entry : bindings.entrySet()) {
+			if (!simplestQEA.isStateFinal(entry.getValue())) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
