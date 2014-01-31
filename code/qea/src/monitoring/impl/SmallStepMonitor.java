49438335aec6aa8b8c31f8c7590edd0e8e5dd1b9
Helena <Helena@VAIO-HELENA>
2014-01-31 02:18:55 +0000

    Method trace commented as there is no Event interface

--- code/qea/src/monitoring/impl/SmallStepMonitor.java
+++ code/qea/src/monitoring/impl/SmallStepMonitor.java
@@ -1,24 +1,21 @@
 package monitoring.impl;
 
-import java.util.List;
-
 import monitoring.intf.Monitor;
-import structure.impl.Verdict;
-import structure.intf.Event;
 
 /**
  * All incremental monitors implement the trace method using the step method.
  * 
- *TODO: Consider using "incremental" instead of "smallstep"
+ * TODO: Consider using "incremental" instead of "smallstep"
  * 
- * @author Giles Reger and Helena Cuenca
+ * @author Giles Reger
+ * @author Helena Cuenca
  */
 
-public abstract class SmallStepMonitor implements Monitor {
+public abstract class SmallStepMonitor extends Monitor {
 
-	public Verdict trace(List<Event> eventList) {
-		// TODO Auto-generated method stub
-		return null;
-	}	
-	
+	// public Verdict trace(List<Event> eventList) {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+
 }
