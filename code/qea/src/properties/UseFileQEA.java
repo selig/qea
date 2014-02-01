5a2e7a4b5691588fb337fc372ff567f7b2f1bba9
Giles <oneandonlygiles@gmail.com>
2014-02-01 20:00:04 +0000

    Added the UseFileQEA with tests that I mentioned previously but forgot to add!

---
+++ code/qea/src/properties/UseFileQEA.java
@@ -0,0 +1,33 @@
+package properties;
+
+import structure.impl.SimplestQEA;
+
+/**
+ * A QEA giving the very simple UseFile property
+ * 
+ * This states that a file should be opened before being used and should eventually be closed.
+ * 
+ * @author Giles Reger
+ */
+
+public class UseFileQEA extends SimplestQEA {
+
+	
+	public UseFileQEA() {
+		// we use two states, three events and 0 as the initial state
+		super(2,3,1);
+		
+		// define the event names for convenience
+		int OPEN = 1;
+		int USE = 2;
+		int CLOSE = 3;
+		
+		// hasnext_true allows us to call next
+		addTransition(1,OPEN,2);
+		addTransition(2,USE,2);
+		addTransition(2,CLOSE,1);
+
+		// only state 1 is final as the file must be closed
+		setStateAsFinal(1);
+	}
+}
