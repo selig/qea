5162a29fd4b1f9e0927e3b6c6e01a3f8b4295dc3
Helena <Helena@VAIO-HELENA>
2014-02-06 00:29:45 +0000

    Method isStateFinal added

--- code/qea/src/structure/intf/QEA.java
+++ code/qea/src/structure/intf/QEA.java
@@ -6,7 +6,6 @@ package structure.intf;
 
 public interface QEA {
 
-	
 	/**
 	 * Retrieves the set of states Q for this QEA
 	 * 
@@ -20,26 +19,34 @@ public interface QEA {
 	 * @return Array of integers with the names of the events
 	 */
 	public int[] getEventsAlphabet();
-	
+
 	/**
-	 * Returns the quantification list in order.
-	 * Variables are positive if universally quantified and negative if existentially quantifed.
+	 * Returns the quantification list in order. Variables are positive if
+	 * universally quantified and negative if existentially quantifed.
 	 * 
 	 * @return Returns Lambda
-	 */		
+	 */
 	public int[] getLambda();
 
 	/**
 	 * Checks if the QEA is deterministic
 	 * 
 	 * @return True if the QEA is deterministic, false otherwise
-	 */				
+	 */
 	public boolean isDeterministic();
-	
+
 	/**
 	 * Checks if the QEA uses free variables
 	 * 
 	 * @return True if the QEA is uses free variables, false otherwise
-	 */		
+	 */
 	public boolean usesFreeVariables();
+
+	/**
+	 * Determines if the specified state is in the set of final states
+	 * 
+	 * @param state
+	 * @return true if the specified state is a final state. Otherwise, false
+	 */
+	public boolean isStateFinal(int state);
 }
