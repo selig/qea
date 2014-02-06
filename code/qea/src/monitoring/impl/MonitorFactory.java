2717339678ffff35a6228e3197c962b720da80b6
Helena <Helena@VAIO-HELENA>
2014-02-06 01:33:53 +0000

    Conditions added to create new monitors

--- code/qea/src/monitoring/impl/MonitorFactory.java
+++ code/qea/src/monitoring/impl/MonitorFactory.java
@@ -6,10 +6,11 @@ import structure.intf.QEA;
 /**
  * This class should be used to construct Monitors from QEA
  * 
- * The general idea is that the structural properties of the QEA are used to select
- * an appropriate Monitor implementation
+ * The general idea is that the structural properties of the QEA are used to
+ * select an appropriate Monitor implementation
  * 
- * @author Giles Reger and Helena Cuenca
+ * @author Giles Reger
+ * @author Helena Cuenca
  */
 
 public class MonitorFactory {
@@ -20,14 +21,25 @@ public class MonitorFactory {
 	 * @param qea
 	 *            The QEA property
 	 * @return A monitor for the QEA property
-	 */	
-	public static Monitor create(QEA qea){
-		
-	// If the QEA is deterministic, does not use free variables and only has one
-	// quantified variable then (currently) use UnoptSimplestSmallStepQEAMonitor
-		if(qea.isDeterministic() && !qea.usesFreeVariables() && qea.getLambda().length==1)
+	 */
+	public static Monitor create(QEA qea) {
+
+		// If the QEA is deterministic, does not use free variables and only has
+		// one quantified variable then (currently) use
+		// SimplestSmallStepQEAMonitor
+		if (qea.isDeterministic() && !qea.usesFreeVariables()
+				&& qea.getLambda().length == 1) {
 			return new SimplestSmallStepQEAMonitor(qea);
-		
+		} else if (qea.isDeterministic() && !qea.usesFreeVariables()
+				&& qea.getLambda().length == 0) {
+			return new SmallStepQEANoQVMonitor(qea); // TODO: Rename this
+		} else if (!qea.isDeterministic() && !qea.usesFreeVariables()
+				&& qea.getLambda().length == 1) {
+			// TODO: New monitor to be created
+		} else if (!qea.isDeterministic() && !qea.usesFreeVariables()
+				&& qea.getLambda().length == 0) {
+			// TODO: New monitor to be created
+		}
 		return null;
 	}
 }
