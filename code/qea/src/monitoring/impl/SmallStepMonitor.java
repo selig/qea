bde3c395246d8f58ad7a451ae1276edd48abfeac
Helena <Helena@VAIO-HELENA>
2014-02-10 12:35:24 +0000

    *Implementation of method 'trace' added

--- code/qea/src/monitoring/impl/SmallStepMonitor.java
+++ code/qea/src/monitoring/impl/SmallStepMonitor.java
@@ -1,6 +1,7 @@
 package monitoring.impl;
 
 import monitoring.intf.Monitor;
+import structure.impl.Verdict;
 import structure.intf.QEA;
 
 /**
@@ -11,14 +12,43 @@ import structure.intf.QEA;
  * @author Giles Reger
  * @author Helena Cuenca
  */
-
 public abstract class SmallStepMonitor<Q extends QEA> extends Monitor<Q> {
 
-	public SmallStepMonitor(Q q){super(q);}
-	
-	// public Verdict trace(List<Event> eventList) {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
+	public SmallStepMonitor(Q q) {
+		super(q);
+	}
+
+	@Override
+	public Verdict trace(int[] eventNames, Object[][] args) {
+		for (int i = 0; i < eventNames.length; i++) {
+			switch (args[i].length) {
+			case 0:
+				step(eventNames[i]);
+				break;
+			case 1:
+				step(eventNames[i], args[i][0]);
+				break;
+			case 2:
+				step(eventNames[i], args[i][0], args[i][1]);
+				break;
+			case 3:
+				step(eventNames[i], args[i][0], args[i][1], args[i][2]);
+				break;
+			case 4:
+				step(eventNames[i], args[i][0], args[i][1], args[i][2],
+						args[i][3]);
+				break;
+			case 5:
+				step(eventNames[i], args[i][0], args[i][1], args[i][2],
+						args[i][3], args[i][4]);
+				break;
+			default:
+				step(eventNames[i], args[i]);
+				break;
+			}
+			step(eventNames[i], args[i]);
+		}
+		return null; // TODO: What's the return value here?
+	}
 
 }
