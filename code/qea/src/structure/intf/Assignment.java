7c4a211a11810368de5a00ebfad2c2e6efb72232
Giles <oneandonlygiles@gmail.com>
2014-02-16 11:19:32 +0000

    Adding Guard and Assignment interfaces as discussed.

---
+++ code/qea/src/structure/intf/Assignment.java
@@ -0,0 +1,12 @@
+package structure.intf;
+
+public interface Assignment {
+
+	/**
+	 * Applying the assignment
+	 * 
+	 * @return updated binding
+	 */			
+	public Binding apply(Binding binding);
+	
+}
