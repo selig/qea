9dad302fd7f8afd25dbe192a72bd77ba0c303bbe
Helena <Helena@VAIO-HELENA>
2014-02-19 15:52:25 +0000

    *Setter for initial state added *Comment added

--- code/qea/src/structure/impl/SimpleQEA.java
+++ code/qea/src/structure/impl/SimpleQEA.java
@@ -5,7 +5,7 @@ import exceptions.ShouldNotHappenException;
 
 public abstract class SimpleQEA implements QEA {
 
-	protected int[] finalStates;
+	protected int[] finalStates; // TODO Can we use a boolean array of here?
 
 	protected final int initialState;
 
