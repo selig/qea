1ddcb420b7059ac59fe28b27386ca4f331dffb13
Helena <Helena@VAIO-HELENA>
2014-02-19 08:42:34 +0000

    *Incremental propositional non-deterministic QEA monitor added

---
+++ code/qea/src/monitoring/impl/monitors/IncrementalPropositionalNonDetQEAMonitor.java
@@ -0,0 +1,60 @@
+package monitoring.impl.monitors;
+
+import monitoring.impl.IncrementalMonitor;
+import monitoring.impl.configs.NonDetConfig;
+import structure.impl.SimpleNonDetQEA;
+import structure.impl.Verdict;
+import exceptions.ShouldNotHappenException;
+
+/**
+ * An incremental propositional monitor for a simple non-deterministic QEA with
+ * no quantified variables
+ * 
+ * @author Helena Cuenca
+ * @author Giles Reger
+ */
+public class IncrementalPropositionalNonDetQEAMonitor extends
+		IncrementalMonitor<SimpleNonDetQEA> {
+
+	/**
+	 * Contains the current configuration (set of states) for the monitor
+	 */
+	private NonDetConfig currentConfig;
+
+	public IncrementalPropositionalNonDetQEAMonitor(SimpleNonDetQEA qea) {
+		super(qea);
+
+		// Set initial state
+		currentConfig.setStates(new int[] { qea.getInitialState() });
+	}
+
+	@Override
+	public Verdict step(int eventName, Object[] args) {
+		throw new ShouldNotHappenException(
+				"Never call propositional QEA with arguments");
+	}
+
+	@Override
+	public Verdict step(int eventName) {
+
+		// Update configuration
+		currentConfig = qea.getNextStates(currentConfig, eventName);
+
+		// Determine verdict according to the current configuration
+		if (qea.containsFinalState(currentConfig)) {
+			return Verdict.WEAK_SUCCESS;
+		}
+		return Verdict.WEAK_FAILURE;
+	}
+
+	@Override
+	public Verdict end() {
+
+		// Determine verdict according to the current configuration
+		if (qea.containsFinalState(currentConfig)) {
+			return Verdict.SUCCESS;
+		}
+		return Verdict.FAILURE;
+	}
+
+}
