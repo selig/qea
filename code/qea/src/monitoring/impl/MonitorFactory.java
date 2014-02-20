fde4d778c61f3e86dc95d3ab58b4dca553cee515
Helena <Helena@VAIO-HELENA>
2014-02-20 10:19:05 +0000

    *Monitors for new cases created

--- code/qea/src/monitoring/impl/MonitorFactory.java
+++ code/qea/src/monitoring/impl/MonitorFactory.java
@@ -1,10 +1,18 @@
 package monitoring.impl;
 
-import java.util.Arrays;
-
-import monitoring.impl.monitors.*;
+import monitoring.impl.monitors.IncrementalNSPropositionalNonDetQEAMonitor;
+import monitoring.impl.monitors.IncrementalNSPropositionalQEAMonitor;
+import monitoring.impl.monitors.IncrementalNonSimpleDetQEAMonitor;
+import monitoring.impl.monitors.IncrementalNonSimpleNonDetQEAMonitor;
+import monitoring.impl.monitors.IncrementalPropositionalNonDetQEAMonitor;
+import monitoring.impl.monitors.IncrementalPropositionalQEAMonitor;
+import monitoring.impl.monitors.IncrementalSimpleDetQEAMonitor;
+import monitoring.impl.monitors.IncrementalSimpleNonDetQEAMonitor;
 import monitoring.intf.Monitor;
-import structure.impl.*;
+import structure.impl.NonSimpleDetQEA;
+import structure.impl.NonSimpleNonDetQEA;
+import structure.impl.SimpleDetQEA;
+import structure.impl.SimpleNonDetQEA;
 import structure.intf.QEA;
 
 /**
@@ -18,7 +26,7 @@ import structure.intf.QEA;
  */
 
 public class MonitorFactory {
-	
+
 	/**
 	 * Constructs a Monitor given a QEA
 	 * 
@@ -27,14 +35,38 @@ public class MonitorFactory {
 	 * @return A monitor for the QEA property
 	 */
 	public static Monitor create(QEA qea) {
-		
-		if(qea instanceof SimpleDetQEA){
-			if(qea.getLambda().length==0) return new IncrementalPropositionalQEAMonitor((SimpleDetQEA) qea);
-			else return new IncrementalSimpleDetQEAMonitor((SimpleDetQEA) qea);
-			
-		} else if (qea instanceof SimpleNonDetQEA){
-			if(qea.getLambda().length==0) return null; // TODO: New monitor to be created
-			else return new IncrementalSimpleNonDetQEAMonitor((SimpleNonDetQEA) qea);
+
+		if (qea instanceof SimpleDetQEA) {
+			if (qea.getLambda().length == 0)
+				return new IncrementalPropositionalQEAMonitor(
+						(SimpleDetQEA) qea);
+			else
+				return new IncrementalSimpleDetQEAMonitor((SimpleDetQEA) qea);
+
+		} else if (qea instanceof SimpleNonDetQEA) {
+			if (qea.getLambda().length == 0)
+				return new IncrementalPropositionalNonDetQEAMonitor(
+						(SimpleNonDetQEA) qea);
+			else
+				return new IncrementalSimpleNonDetQEAMonitor(
+						(SimpleNonDetQEA) qea);
+		} else if (qea instanceof NonSimpleDetQEA) {
+			if (qea.getLambda().length == 0) {
+				return new IncrementalNSPropositionalQEAMonitor(
+						(NonSimpleDetQEA) qea);
+			} else {
+				return new IncrementalNonSimpleDetQEAMonitor(
+						(NonSimpleDetQEA) qea);
+			}
+		} else if (qea instanceof NonSimpleNonDetQEA) {
+			if (qea.getLambda().length == 0) {
+				return new IncrementalNSPropositionalNonDetQEAMonitor(
+						(NonSimpleNonDetQEA) qea);
+			} else {
+				return new IncrementalNonSimpleNonDetQEAMonitor(
+						(NonSimpleNonDetQEA) qea);
+			}
+
 		} else if (!qea.isDeterministic() && !qea.usesFreeVariables()
 				&& qea.getLambda().length == 0) {
 			// TODO: New monitor to be created
