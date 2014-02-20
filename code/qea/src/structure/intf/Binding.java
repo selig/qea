264132ec849b09fb7def1cedc736405cad581e59
Helena <Helena@VAIO-HELENA>
2014-02-20 01:08:45 +0000

    *Signature added for methods getValue and setValue

--- code/qea/src/structure/intf/Binding.java
+++ code/qea/src/structure/intf/Binding.java
@@ -2,4 +2,22 @@ package structure.intf;
 
 public interface Binding {
 
+	/**
+	 * Returns the value of the variable with the specified name
+	 * 
+	 * @param variableName
+	 *            Variable name
+	 * @return Value of the variable
+	 */
+	public Object getValue(int variableName);
+
+	/**
+	 * Sets the value of the variable with the specified name
+	 * 
+	 * @param variableName
+	 *            Variable name
+	 * @param value
+	 *            Value of the variable
+	 */
+	public void setValue(int variableName, Object value);
 }
