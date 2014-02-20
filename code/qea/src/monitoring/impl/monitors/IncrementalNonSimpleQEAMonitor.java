7ec811b1ca0c9cf7de5c43501b4704f2485f1ed7
Helena <Helena@VAIO-HELENA>
2014-02-20 09:58:45 +0000

    *Method end added

--- code/qea/src/monitoring/impl/monitors/IncrementalNonSimpleQEAMonitor.java
+++ code/qea/src/monitoring/impl/monitors/IncrementalNonSimpleQEAMonitor.java
@@ -2,12 +2,25 @@ package monitoring.impl.monitors;
 
 import monitoring.impl.IncrementalMonitor;
 import structure.impl.NonSimpleQEA;
+import structure.impl.Verdict;
 
 public abstract class IncrementalNonSimpleQEAMonitor<Q extends NonSimpleQEA>
 		extends IncrementalMonitor<Q> {
 
 	public IncrementalNonSimpleQEAMonitor(Q q) {
 		super(q);
+	}
+
+	@Override
+	public Verdict end() {
+
+		// According to the quantification of the variable, return verdict
+		if (qea.isQuantificationUniversal() && allBindingsInFinalState()
+				|| !qea.isQuantificationUniversal()
+				&& existsOneBindingInFinalState()) {
+			return Verdict.SUCCESS;
+		}
+		return Verdict.FAILURE;
 	}
 
 }
