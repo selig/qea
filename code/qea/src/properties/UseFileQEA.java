ec69d3b139198683a38b1f39dccbc8022986eee6
Helena <Helena@VAIO-HELENA>
2014-02-20 01:01:25 +0000

    *Task to add quantification removed. It's already included as a parameter of the constructor *Method addTransition renamed to addTransitions *Modification to call new method addTransition(int, int, int)

--- code/qea/src/properties/UseFileQEA.java
+++ code/qea/src/properties/UseFileQEA.java
@@ -1,34 +1,32 @@
 package properties;
 
+import static structure.impl.Quantification.FORALL;
 import structure.impl.SimpleDetQEA;
-import static structure.impl.Quantification.*;
 
 /**
  * A QEA giving the very simple UseFile property
  * 
- * This states that a file should be opened before being used and should eventually be closed.
+ * This states that a file should be opened before being used and should
+ * eventually be closed.
  * 
  * @author Giles Reger
  */
 
 public class UseFileQEA extends SimpleDetQEA {
 
-	
 	public UseFileQEA() {
 		// we use two states, three events and 0 as the initial state
-		super(2,3,1,FORALL);
-		
-		//TODO add flag for quantified
-		
+		super(2, 3, 1, FORALL);
+
 		// define the event names for convenience
 		int OPEN = 1;
 		int USE = 2;
 		int CLOSE = 3;
-		
+
 		// hasnext_true allows us to call next
-		addTransition(1,OPEN,2);
-		addTransition(2,USE,2);
-		addTransition(2,CLOSE,1);
+		addTransition(1, OPEN, 2);
+		addTransition(2, USE, 2);
+		addTransition(2, CLOSE, 1);
 
 		// only state 1 is final as the file must be closed
 		setStateAsFinal(1);
