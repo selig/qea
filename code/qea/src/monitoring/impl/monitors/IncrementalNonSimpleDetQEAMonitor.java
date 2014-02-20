00fe1e66b6a977543770d55a956ba81ab757622c
Helena <Helena@VAIO-HELENA>
2014-02-20 09:55:44 +0000

    *First implementation of step(int, Object[]) added. Pending tasks *Method toString overridden

--- code/qea/src/monitoring/impl/monitors/IncrementalNonSimpleDetQEAMonitor.java
+++ code/qea/src/monitoring/impl/monitors/IncrementalNonSimpleDetQEAMonitor.java
@@ -1,32 +1,127 @@
 package monitoring.impl.monitors;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+import monitoring.impl.configs.DetConfig;
 import structure.impl.NonSimpleDetQEA;
 import structure.impl.Verdict;
 
+/**
+ * An incremental monitor for a non-simple deterministic QEA
+ * 
+ * @author Helena Cuenca
+ * @author Giles Reger
+ */
 public class IncrementalNonSimpleDetQEAMonitor extends
 		IncrementalNonSimpleQEAMonitor<NonSimpleDetQEA> {
 
-	public IncrementalNonSimpleDetQEAMonitor(NonSimpleDetQEA q) {
-		super(q);
-		// TODO Auto-generated constructor stub
+	/**
+	 * Maps the current values of the quantified variable to the deterministic
+	 * configuration for each binding. The configuration contains the state and
+	 * the bindings for the free variables
+	 */
+	private IdentityHashMap<Object, DetConfig> bindings;
+
+	/**
+	 * Creates an IncrementalNonSimpleDetQEAMonitor for the specified QEA
+	 * 
+	 * @param qea
+	 *            QEA
+	 */
+	public IncrementalNonSimpleDetQEAMonitor(NonSimpleDetQEA qea) {
+		super(qea);
+		bindings = new IdentityHashMap<>();
 	}
 
 	@Override
 	public Verdict step(int eventName, Object[] args) {
-		// TODO Auto-generated method stub
-		return null;
+
+		boolean existingBinding = false;
+		DetConfig config;
+
+		// Obtain the value for the quantified variable
+		// TODO How can we obtain the quantified variable value if the
+		// definition of the parameters is specific to a start state/event in
+		// the QEA?
+		Object quantifiedVar = null;
+
+		// TODO If there's no quantified variable, the event should be applied
+		// to all the bindings?
+
+		// Determine if the value received corresponds to an existing binding
+		if (bindings.containsKey(quantifiedVar)) { // Existing quantified
+													// variable binding
+
+			// Get current configuration for the binding
+			config = bindings.get(quantifiedVar);
+
+			// Assign flag for counters update
+			existingBinding = true;
+
+		} else { // New quantified variable binding
+
+			// Create configuration for the new binding
+			config = new DetConfig(qea.getInitialState());
+			config.setBinding(qea.newBinding());
+		}
+
+		// Flag needed to update counters later
+		boolean startConfigFinal = qea.isStateFinal(config.getState());
+
+		// Compute next configuration
+		config = qea.getNextConfig(config, eventName, args);
+
+		// Flag needed to update counters later
+		boolean endConfigFinal = qea.isStateFinal(config.getState());
+
+		// Update/add configuration for the binding
+		bindings.put(quantifiedVar, config);
+
+		// If applicable, update counters
+		// TODO Consider extracting the update counters logic in another method
+		if (existingBinding) {
+			if (startConfigFinal && !endConfigFinal) {
+				bindingsInNonFinalStateCount++;
+				bindingsInFinalStateCount--;
+			} else if (!startConfigFinal && endConfigFinal) {
+				bindingsInNonFinalStateCount--;
+				bindingsInFinalStateCount++;
+			}
+		} else {
+			if (endConfigFinal) {
+				bindingsInFinalStateCount++;
+			} else {
+				bindingsInNonFinalStateCount++;
+			}
+		}
+
+		// According to the quantification of the variable, return verdict
+		if (qea.isQuantificationUniversal() && allBindingsInFinalState()
+				|| !qea.isQuantificationUniversal()
+				&& existsOneBindingInFinalState()) {
+			return Verdict.WEAK_SUCCESS;
+		}
+		return Verdict.WEAK_FAILURE;
 	}
 
 	@Override
 	public Verdict step(int eventName) {
-		// TODO Auto-generated method stub
-		return null;
+		Verdict finalVerdict = null;
+		for (Map.Entry<Object, DetConfig> entry : bindings.entrySet()) {
+			finalVerdict = step(eventName, entry.getKey());
+		}
+		return finalVerdict;
 	}
 
 	@Override
-	public Verdict end() {
-		// TODO Auto-generated method stub
-		return null;
+	public String toString() {
+		String ret = "Map:\n";
+		for (IdentityHashMap.Entry<Object, DetConfig> entry : bindings
+				.entrySet()) {
+			ret += entry.getKey() + "\t->\t" + entry.getValue() + "\n";
+		}
+		return ret;
 	}
 
 }
