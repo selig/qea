0be3d806f8a1592726d46bfed0ab1d6e7430d429
Helena <Helena@10.244.203.1>
2014-02-27 14:12:31 +0000

    Setter methods added

--- code/qea/src/monitoring/impl/configs/NonDetConfig.java
+++ code/qea/src/monitoring/impl/configs/NonDetConfig.java
@@ -36,11 +36,39 @@ public class NonDetConfig implements Configuration {
 		this.states = states;
 	}
 
+	public void setBindings(Binding[] bindings) {
+		this.bindings = bindings;
+	}
+
+	/**
+	 * Replaces the state at the specified position in the array of states
+	 * 
+	 * @param index
+	 *            Index of the state to replace
+	 * @param state
+	 *            State to be stored at the specified position
+	 */
+	public void setState(int index, int state) {
+		states[index] = state;
+	}
+
+	/**
+	 * Replaces the binding at the specified position in the array of bindings
+	 * 
+	 * @param index
+	 *            Index of the binding to replace
+	 * @param binding
+	 *            Binding to be stored at the specified position
+	 */
+	public void setBinding(int index, Binding binding) {
+		bindings[index] = binding;
+	}
+
 	/*
 	 * Override toString to print out config
 	 */
 	@Override
-	public String toString(){
+	public String toString() {
 		// currently ignores bindings
 		return Arrays.toString(states);
 	}
