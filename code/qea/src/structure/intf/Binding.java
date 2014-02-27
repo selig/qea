2b01e023878eb4ee34edcf9040107fe6ebd966f1
Helena <Helena@10.244.203.1>
2014-02-27 14:22:40 +0000

    Signature of method setEmpty added

--- code/qea/src/structure/intf/Binding.java
+++ code/qea/src/structure/intf/Binding.java
@@ -22,35 +22,49 @@ public abstract class Binding {
 	 *            Value of the variable
 	 */
 	public abstract void setValue(int variableName, Object value);
-	
-	
+
 	public abstract Binding copy();
-	
-	
-	/*
+
+	/**
+	 * Removes the values for the current binding, leaving it as an empty
+	 * binding
+	 */
+	public abstract void setEmpty();
+
+	/**
 	 * Helper method for getting the value of a variable and throwing an
 	 * exception if the value is not there
 	 * 
-	 * @return the value of var in binding
+	 * @param var
+	 *            Name of the variable
+	 * @return Value of var in binding
 	 */
-	public Object getForced(int var){
+	public Object getForced(int var) {
 		Object val = this.getValue(var);
-		if(val==null) throw new BindingException("Variable x_"+var+" expected in "+this);
+		if (val == null)
+			throw new BindingException("Variable x_" + var + " expected in "
+					+ this);
 		return val;
 	}
-	/*
-	 * Helper method for getting the value of a variable as an integer and throwing an
-	 * exception if the value is not there or is not an integer
+
+	/**
+	 * Helper method for getting the value of a variable as an integer and
+	 * throwing an exception if the value is not there or is not an integer
 	 * 
+	 * @param var
+	 *            Name of the variable
 	 * @return the value of var in binding
 	 */
-	public Integer getForcedAsInteger(int var){
+	public Integer getForcedAsInteger(int var) {
 		Object val = this.getValue(var);
-		if(val==null) throw new BindingException("Variable x_"+var+" expected in "+this);
-		if(!(val instanceof Integer)) 
-			throw new BindingException("Variable x_"+var+" expected as integer in "+this);
-		
+		if (val == null)
+			throw new BindingException("Variable x_" + var + " expected in "
+					+ this);
+		if (!(val instanceof Integer))
+			throw new BindingException("Variable x_" + var
+					+ " expected as integer in " + this);
+
 		return (Integer) val;
-	}		
-	
+	}
+
 }
