c8cf236b7e09f7aec60879b7780fbc160fb1d5dc
Helena <Helena@10.244.203.1>
2014-02-28 12:11:51 +0000

    Constructor added that initialises the state and binding

--- code/qea/src/monitoring/impl/configs/NonDetConfig.java
+++ code/qea/src/monitoring/impl/configs/NonDetConfig.java
@@ -3,6 +3,7 @@ package monitoring.impl.configs;
 import java.util.Arrays;
 
 import monitoring.intf.Configuration;
+import structure.impl.BindingImpl;
 import structure.intf.Binding;
 
 /**
@@ -22,6 +23,15 @@ public class NonDetConfig implements Configuration {
 	public NonDetConfig(int initialState) {
 		states = new int[1];
 		states[0] = initialState;
+	}
+	
+	// TODO Create the analogue constructor for DetConfig
+	public NonDetConfig(int initialState, Binding binding) {
+		states = new int[1];
+		states[0] = initialState;
+		
+		bindings = new BindingImpl[1];
+		bindings[0] = binding;
 	}
 
 	public int[] getStates() {
