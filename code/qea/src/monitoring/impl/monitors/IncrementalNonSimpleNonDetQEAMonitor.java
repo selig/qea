7ce699f4360a8b6c623051a86ddc8445a053182b
Helena <Helena@10.244.203.1>
2014-02-28 12:12:06 +0000

    Method step(int eventName, Object[] args) implemented

--- code/qea/src/monitoring/impl/monitors/IncrementalNonSimpleNonDetQEAMonitor.java
+++ code/qea/src/monitoring/impl/monitors/IncrementalNonSimpleNonDetQEAMonitor.java
@@ -1,20 +1,88 @@
 package monitoring.impl.monitors;
 
+import java.util.IdentityHashMap;
+
+import monitoring.impl.configs.NonDetConfig;
 import structure.impl.NonSimpleNonDetQEA;
 import structure.impl.Verdict;
 
+/**
+ * 
+ * @author Helena Cuenca
+ * @author Giles Reger
+ */
 public class IncrementalNonSimpleNonDetQEAMonitor extends
 		IncrementalNonSimpleQEAMonitor<NonSimpleNonDetQEA> {
 
-	public IncrementalNonSimpleNonDetQEAMonitor(NonSimpleNonDetQEA q) {
-		super(q);
-		// TODO Auto-generated constructor stub
+	private IdentityHashMap<Object, NonDetConfig> bindings;
+
+	public IncrementalNonSimpleNonDetQEAMonitor(NonSimpleNonDetQEA qea) {
+		super(qea);
+		bindings = new IdentityHashMap<>();
 	}
 
 	@Override
 	public Verdict step(int eventName, Object[] args) {
-		// TODO Auto-generated method stub
-		return null;
+
+		boolean existingBinding = false;
+		NonDetConfig config;
+
+		// Obtain the value for the quantified variable
+		// Assumption: The (unique) quantified variable is present in all events
+		// and it’s always the first argument
+		Object quantifiedVar = args[0];
+
+		// Determine if the value received corresponds to an existing binding
+		if (bindings.containsKey(quantifiedVar)) { // Existing quantified
+													// variable binding
+			// Get current configuration for the binding
+			config = bindings.get(quantifiedVar);
+
+			// Assign flag for counters update
+			existingBinding = true;
+
+		} else { // New quantified variable binding
+
+			// Create configuration for the new binding
+			config = new NonDetConfig(qea.getInitialState(), qea.newBinding());
+		}
+
+		// Flag needed to update counters later
+		boolean startConfigFinal = qea.containsFinalState(config);
+
+		// Compute next configuration
+		config = qea.getNextConfig(config, eventName, args);
+
+		// Flag needed to update counters later
+		boolean endConfigFinal = qea.containsFinalState(config);
+
+		// Update/add configuration for the binding
+		bindings.put(quantifiedVar, config);
+
+		// If applicable, update counters
+		if (existingBinding) {
+			if (startConfigFinal && !endConfigFinal) {
+				bindingsInNonFinalStateCount++;
+				bindingsInFinalStateCount--;
+			} else if (!startConfigFinal && endConfigFinal) {
+				bindingsInNonFinalStateCount--;
+				bindingsInFinalStateCount++;
+			}
+		} else {
+			if (endConfigFinal) {
+				bindingsInFinalStateCount++;
+			} else {
+				bindingsInNonFinalStateCount++;
+			}
+		}
+
+		// According to the quantification of the variable, return verdict
+		if (qea.isQuantificationUniversal() && allBindingsInFinalState()
+				|| !qea.isQuantificationUniversal()
+				&& existsOneBindingInFinalState()) {
+			return Verdict.WEAK_SUCCESS;
+		}
+		return Verdict.WEAK_FAILURE;
 	}
 
 	@Override
