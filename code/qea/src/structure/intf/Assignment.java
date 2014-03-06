351009fdd3794ab273c9e26e77ef1884b4feed1d
Helena <Helena@10.244.203.1>
2014-03-06 22:27:06 +0000

    Clean up

--- code/qea/src/structure/intf/Assignment.java
+++ code/qea/src/structure/intf/Assignment.java
@@ -1,6 +1,5 @@
 package structure.intf;
 
-import exceptions.BindingException;
 
 public abstract class Assignment {
 
@@ -10,34 +9,39 @@ public abstract class Assignment {
 	 * Assignments should always create a *new* binding
 	 * 
 	 * @return updated binding
-	 */			
+	 */
 	public abstract Binding apply(Binding binding);
-	
+
 	private final String name;
-	
-	public Assignment(String name){ this.name=name; }
-	
+
+	public Assignment(String name) {
+		this.name = name;
+	}
+
 	/*
 	 * Get the name of the assignment
 	 * 
 	 * @return the name of the assignment
 	 */
-	public String getName(){ return name;}	
-	
+	public String getName() {
+		return name;
+	}
+
 	/*
 	 * Produce an assignment capturing binding += [var0 -> binding[var1]]
 	 * 
 	 * @ returns an assignment for this property
 	 */
-	public static Assignment store(final int var0, final int var1){
-		return new Assignment("store(x_"+var0+", x_"+var1+")"){
-			public Binding apply(Binding binding){
-				Object val1 = binding.getForced(var1);	
+	public static Assignment store(final int var0, final int var1) {
+		return new Assignment("store(x_" + var0 + ", x_" + var1 + ")") {
+			@Override
+			public Binding apply(Binding binding) {
+				Object val1 = binding.getForced(var1);
 				Binding newBinding = binding.copy();
 				binding.setValue(var0, val1);
 				return newBinding;
 			}
 		};
-	}		
-	
+	}
+
 }
--- code/qea/src/structure/intf/Guard.java
+++ code/qea/src/structure/intf/Guard.java
@@ -1,84 +1,89 @@
 package structure.intf;
 
-import exceptions.BindingException;
 
 public abstract class Guard {
 
-	
 	/**
 	 * Checking the guard
 	 * 
 	 * @return true if guard evaluates to true on binding
-	 */	
+	 */
 	public abstract boolean check(Binding binding);
-	
+
 	private final String name;
-	
-	public Guard(String name){ this.name=name; }
-	
+
+	public Guard(String name) {
+		this.name = name;
+	}
+
 	/*
 	 * Get the name of the guard
 	 * 
 	 * @return the name of the guard
 	 */
-	public String getName(){ return name;}
-	
+	public String getName() {
+		return name;
+	}
 
-	
 	/*
 	 * Produce a guard capturing binding(var0) == binding(var1)
 	 * 
 	 * @ returns a guard for this property
 	 */
-	public static Guard isEqual(final int var0, final int var1){
-		return new Guard("x_"+var0+" == x_"+var1){
-			public boolean check(Binding binding){
+	public static Guard isEqual(final int var0, final int var1) {
+		return new Guard("x_" + var0 + " == x_" + var1) {
+			@Override
+			public boolean check(Binding binding) {
 				Object val0 = binding.getForced(var0);
-				Object val1 = binding.getForced(var1);													
+				Object val1 = binding.getForced(var1);
 				return (val0 == val1);
 			}
 		};
 	}
+
 	/*
 	 * Produce a guard capturing binding(var0) != binding(var1)
 	 * 
 	 * @ returns a guard for this property
 	 */
-	public static Guard isNotEqual(final int var0, final int var1){
-		return new Guard("x_"+var0+" != x_"+var1){
-			public boolean check(Binding binding){
+	public static Guard isNotEqual(final int var0, final int var1) {
+		return new Guard("x_" + var0 + " != x_" + var1) {
+			@Override
+			public boolean check(Binding binding) {
 				Object val0 = binding.getForced(var0);
-				Object val1 = binding.getForced(var1);													
+				Object val1 = binding.getForced(var1);
 				return (val0 != val1);
 			}
 		};
-	}	
-	
+	}
+
 	/*
 	 * Produce a guard capturing binding(var0) equals binding(var1)
 	 * 
 	 * @ returns a guard for this property
 	 */
-	public static Guard isEqualSem(final int var0, final int var1){
-		return new Guard("x_"+var0+" equals x_"+var1){
-			public boolean check(Binding binding){
+	public static Guard isEqualSem(final int var0, final int var1) {
+		return new Guard("x_" + var0 + " equals x_" + var1) {
+			@Override
+			public boolean check(Binding binding) {
 				Object val0 = binding.getForced(var0);
-				Object val1 = binding.getForced(var1);												
+				Object val1 = binding.getForced(var1);
 				return val0.equals(val1);
 			}
 		};
-	}	
-	
+	}
+
 	/*
 	 * Produce a guard capturing binding(var0) > binding(var1)
 	 * 
 	 * @ returns a guard for this property
 	 */
-	public static Guard isGreaterThan(final int var0, final int var1){
-		return new Guard("x_"+var0+" > x_"+var1){
-			public boolean check(Binding binding){
+	public static Guard isGreaterThan(final int var0, final int var1) {
+		return new Guard("x_" + var0 + " > x_" + var1) {
+			@Override
+			public boolean check(Binding binding) {
 				Integer val0 = binding.getForcedAsInteger(var0);
-				Integer val1 = binding.getForcedAsInteger(var1);													
+				Integer val1 = binding.getForcedAsInteger(var1);
 				return (val0 > val1);
 			}
 		};
@@ -89,14 +94,15 @@ public abstract class Guard {
 	 * 
 	 * @ returns a guard for this property
 	 */
-	public static Guard isLessThanOrEqualTo(final int var0, final int var1){
-		return new Guard("x_"+var0+" <= x_"+var1){
-			public boolean check(Binding binding){
+	public static Guard isLessThanOrEqualTo(final int var0, final int var1) {
+		return new Guard("x_" + var0 + " <= x_" + var1) {
+			@Override
+			public boolean check(Binding binding) {
 				Integer val0 = binding.getForcedAsInteger(var0);
-				Integer val1 = binding.getForcedAsInteger(var1);													
+				Integer val1 = binding.getForcedAsInteger(var1);
 				return (val0 <= val1);
 			}
 		};
-	}	
-	
+	}
+
 }
