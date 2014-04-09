bf4937b57f7337bbe57a9e675c0dacf6c19dde72
Helena <Helena@10.244.196.241>
2014-04-09 16:31:21 +0100

    Tests added for ResourceLifecycle property with different QEA

---
+++ code/qea/src/benchmark/rovers/ResourceLifecycleQeaDoEval.java
@@ -0,0 +1,44 @@
+package benchmark.rovers;
+
+import properties.rovers.ResourceLifecycle_QVar01_FVar_Det_QEA;
+import properties.rovers.ResourceLifecycle_QVar01_FVar_NonDet_QEA;
+import properties.rovers.ResourceLifecycle_QVar01_NoFVar_NonDet_QEA;
+import properties.rovers.RoverCaseStudy;
+
+public class ResourceLifecycleQeaDoEval extends DoEval {
+
+	@Override
+	public DoWork makeWork() {
+		return new QeaDoWork();
+	}
+
+	public static void main(String[] args) {
+
+		ResourceLifecycleQeaDoEval eval = new ResourceLifecycleQeaDoEval();
+
+		QeaDoWork.print = false;
+
+		// Worst case
+		eval.eval_for_ResourceLifecycle(
+				RoverCaseStudy.makeResourceLifecycleGeneral(),
+				"ResourceLifecycle");
+
+		// Intermediate cases
+		eval.eval_for_ResourceLifecycle(
+				new ResourceLifecycle_QVar01_FVar_NonDet_QEA(),
+				"ResourceLifecycle");
+
+		eval.eval_for_ResourceLifecycle(
+				new ResourceLifecycle_QVar01_FVar_Det_QEA(),
+				"ResourceLifecycle");
+
+		eval.eval_for_ResourceLifecycle(
+				new ResourceLifecycle_QVar01_NoFVar_NonDet_QEA(),
+				"ResourceLifecycle");
+
+		// Best case
+		eval.eval_for_ResourceLifecycle(RoverCaseStudy.makeResourceLifecycle(),
+				"ResourceLifecycle");
+
+	}
+}
