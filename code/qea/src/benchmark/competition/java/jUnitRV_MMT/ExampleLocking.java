e7c13e12f259cacbeeb6f03766f41294e0323d00
Helena <Helena@10.180.3.125>
2014-06-04 15:50:45 +0100

    Instrumentation for QEA

--- code/qea/src/benchmark/competition/java/jUnitRV_MMT/ExampleLocking.java
+++ code/qea/src/benchmark/competition/java/jUnitRV_MMT/ExampleLocking.java
@@ -7,14 +7,30 @@ import java.util.concurrent.Future;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import monitoring.impl.MonitorFactory;
+import monitoring.intf.Monitor;
+import properties.Property;
+import properties.competition.JavaRV_mmt;
+import structure.intf.QEA;
+
 // Java - Team3 - Bench3
 
 public class ExampleLocking implements Runnable {
 
+	private static QEA qea;
+	private static Monitor monitor;
+	private static int RUN;
+	private static int LOCK_TRUE;
+	private static int UNLOCK;
+	private static int ACTION;
+
 	public static void main(String... argv) throws ExecutionException,
 			InterruptedException {
-		ExecutorService executor = Executors.newFixedThreadPool(5);
-		int n = 5;
+
+		setUpQEAMonitoring();
+//		int n = 5;
+		int n = 2;
+		ExecutorService executor = Executors.newFixedThreadPool(n);
 		Future<?>[] futures = new Future<?>[n];
 		for (int i = 0; i < n; ++i) {
 			futures[i] = executor.submit(new ExampleLocking(i));
@@ -32,8 +48,10 @@ public class ExampleLocking implements Runnable {
 
 	@Override
 	public void run() {
+		System.out.println(this.id + " " + monitor.step(RUN, this.id));
 		for (int i = 0; i < 1000;) {
 			if (lock()) {
+				System.out.println(this.id + " " + monitor.step(LOCK_TRUE, this.id));
 				try {
 					action();
 					Thread.sleep(5);
@@ -52,14 +70,25 @@ public class ExampleLocking implements Runnable {
 
 	public void action() {
 		System.out.println(id);
+		System.out.println(this.id + " " + monitor.step(ACTION, this.id));
 	}
 
 	public void unlock() {
 		lock.unlock();
+		System.out.println(this.id + " " + monitor.step(UNLOCK, this.id));
 	}
 
 	private static final Lock lock = new ReentrantLock();
 
 	private final int id;
+
+	public static void setUpQEAMonitoring() {
+		qea = new JavaRV_mmt().make(Property.JAVARV_MMT_THREE);
+		monitor = MonitorFactory.create(qea);
+		RUN = qea.get_event_id("run");
+		LOCK_TRUE = qea.get_event_id("lock_true");
+		UNLOCK = qea.get_event_id("unlock");
+		ACTION = qea.get_event_id("action");
+	}
 
 }
