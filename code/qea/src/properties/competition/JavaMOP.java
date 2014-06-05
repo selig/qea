04c20e41791158c943b48a5d3845e19fb954c6e6
Helena <Helena@10.180.3.74>
2014-06-05 19:37:55 +0100

    Property three added

--- code/qea/src/properties/competition/JavaMOP.java
+++ code/qea/src/properties/competition/JavaMOP.java
@@ -1,77 +1,142 @@
 package properties.competition;
 
 import static structure.impl.other.Quantification.FORALL;
-import creation.QEABuilder;
 import properties.Property;
 import properties.PropertyMaker;
-import properties.papers.*;
+import properties.papers.DaCapo;
+import properties.papers.HasNextQEA;
 import structure.intf.Assignment;
 import structure.intf.Guard;
 import structure.intf.QEA;
+import creation.QEABuilder;
 
 public class JavaMOP implements PropertyMaker {
 
 	@Override
 	public QEA make(Property property) {
-		switch(property){
-			case STEPR_ONE : return makeOne();
-			case STEPR_TWO : return makeTwo();
-			case STEPR_THREE : return makeThree();
-			case STEPR_FOUR : return makeFour();
-			case STEPR_FIVE : return makeFive();
+		switch (property) {
+		case STEPR_ONE:
+			return makeOne();
+		case STEPR_TWO:
+			return makeTwo();
+		case STEPR_THREE:
+			return makeThree();
+		case STEPR_FOUR:
+			return makeFour();
+		case STEPR_FIVE:
+			return makeFive();
 		}
 		return null;
 	}
 
-	public QEA makeOne(){
+	public QEA makeOne() {
 		// This is just the HasNext property we know and love
 		return new HasNextQEA();
 	}
-	
-	public QEA makeTwo(){
-		
+
+	public QEA makeTwo() {
+
 		/*
-		 * Note - this has a disjoint alphabet, we should implement this optimisation!
+		 * Note - this has a disjoint alphabet, we should implement this
+		 * optimisation!
 		 */
-		
+
 		QEABuilder q = new QEABuilder("SafeLock");
-		
+
 		int LOCK = 1;
 		int UNLOCK = 2;
 		int thread = -1;
 		int lock = -2;
 		int count = 1;
-		
+
 		q.addQuantification(FORALL, thread);
 		q.addQuantification(FORALL, lock);
-		
-		q.addTransition(1,LOCK,new int[]{thread,lock},Assignment.set(count, 1),2);
-		q.addTransition(2,LOCK,new int[]{thread,lock},Assignment.increment(count),2);
-		q.addTransition(2,UNLOCK,new int[]{thread,lock},
-				Guard.isGreaterThanConstant(count, 1),Assignment.decrement(count),2);
-		q.addTransition(2,UNLOCK,new int[]{thread,lock},
-				Guard.isSemEqualToConstant(count,1),Assignment.set(count, 0),1);
-		
-		q.addFinalStates(1,2);
-		
+
+		q.addTransition(1, LOCK, new int[] { thread, lock },
+				Assignment.set(count, 1), 2);
+		q.addTransition(2, LOCK, new int[] { thread, lock },
+				Assignment.increment(count), 2);
+		q.addTransition(2, UNLOCK, new int[] { thread, lock },
+				Guard.isGreaterThanConstant(count, 1),
+				Assignment.decrement(count), 2);
+		q.addTransition(2, UNLOCK, new int[] { thread, lock },
+				Guard.isSemEqualToConstant(count, 1), Assignment.set(count, 0),
+				1);
+
+		q.addFinalStates(1, 2);
+
 		QEA qea = q.make();
 		qea.record_event_name("lock", LOCK);
 		qea.record_event_name("unlock", UNLOCK);
 		return qea;
 	}
-	
-	public QEA makeThree(){
-		//TODO - the A^n B^n C^n one
-		return null;
+
+	public QEA makeThree() {
+		// TODO - the A^n B^n C^n one
+
+		QEABuilder q = new QEABuilder("A^n B^n C^n");
+
+		int A = 1;
+		int B = 2;
+		int C = 3;
+		int a = 1;
+		int b = 2;
+
+		q.startTransition(1);
+		q.eventName(A);
+		q.addAssignment(Assignment.set(a, 1));
+		q.endTransition(2);
+
+		q.startTransition(2);
+		q.eventName(A);
+		q.addAssignment(Assignment.increment(a));
+		q.endTransition(2);
+
+		q.startTransition(2);
+		q.eventName(B);
+		q.addAssignment(Assignment.set(b, 1));
+		q.endTransition(3);
+
+		q.startTransition(3);
+		q.eventName(B);
+		q.addGuard(Guard.isLessThanOrEqualTo(b, a));
+		q.addAssignment(Assignment.increment(b));
+		q.endTransition(3);
+
+		q.startTransition(3);
+		q.eventName(C);
+		q.addGuard(Guard.isEqual(a, b));
+		q.addAssignment(Assignment.decrement(b));
+		q.endTransition(4);
+
+		q.startTransition(4);
+		q.eventName(C);
+		q.addGuard(Guard.varIsGreaterThanVal(b, 0));
+		q.addAssignment(Assignment.decrement(b));
+		q.endTransition(4);
+
+		q.startTransition(4);
+		q.eventName(C);
+		q.addGuard(Guard.varIsEqualToIntVal(b, 0));
+		q.endTransition(5);
+
+		q.addFinalStates(5);
+
+		QEA qea = q.make();
+		qea.record_event_name("a", A);
+		qea.record_event_name("b", B);
+		qea.record_event_name("c", C);
+
+		return qea;
 	}
-	
-	public QEA makeFour(){
+
+	public QEA makeFour() {
 		return DaCapo.makeUnsafeMapIter();
 	}
-	
-	public QEA makeFive(){
+
+	public QEA makeFive() {
 		// There isn't one
 		return null;
-	}	
-	
+	}
+
 }
