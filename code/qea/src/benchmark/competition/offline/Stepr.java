545d0f5b4d302f10bb3fc309f7650b5c20ac27cc
hcuenca <helena.cuenca@gmail.com>
2014-07-17 17:15:59 +0100

    Benchmark for STePr 1 created

--- code/qea/src/benchmark/competition/offline/Stepr.java
+++ code/qea/src/benchmark/competition/offline/Stepr.java
@@ -4,47 +4,73 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.xml.sax.SAXException;
-
 import monitoring.impl.MonitorFactory;
 import monitoring.impl.XMLFileMonitorSAX;
 import monitoring.impl.translators.OfflineTranslator;
+
+import org.xml.sax.SAXException;
+
 import structure.impl.other.Verdict;
 import structure.intf.QEA;
 
 public class Stepr {
 
-	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException{
-		//runTwo();
+	public static void main(String[] args) throws IOException,
+			ParserConfigurationException, SAXException {
+		// runOne();
+		// runTwo();
 		runThree();
-		//runFour();
+		// runFour();
 	}
-	
-	static String trace = "/Users/giles/git/csrv14/OFFLINE/Team6/log.xml";
-	
-	public static void runTwo() throws IOException, ParserConfigurationException, SAXException{				
+
+	static String trace = "traces/Team6/log.xml";
+
+	public static void runOne() throws IOException,
+			ParserConfigurationException, SAXException {
+		QEA qea = (new properties.competition.Stepr()).makeOne();
+		System.err.println("Running with "
+				+ MonitorFactory.create(qea).getClass());
+		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
+				.makeOne();
+		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
+		Verdict v = m.monitor();
+		System.err.println("Verdict for one was " + v);
+	}
+
+	public static void runTwo() throws IOException,
+			ParserConfigurationException, SAXException {
 		QEA qea = (new properties.competition.Stepr()).makeTwo();
-		System.err.println("Running with "+MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators()).makeTwo();
-		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace,qea,t);
+		System.err.println("Running with "
+				+ MonitorFactory.create(qea).getClass());
+		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
+				.makeTwo();
+		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
 		Verdict v = m.monitor();
-		System.err.println("Verdict for two was "+v);
+		System.err.println("Verdict for two was " + v);
 	}
-	public static void runThree() throws IOException, ParserConfigurationException, SAXException{				
+
+	public static void runThree() throws IOException,
+			ParserConfigurationException, SAXException {
 		QEA qea = (new properties.competition.Stepr()).makeThree();
-		System.err.println("Running with "+MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators()).makeThree();
-		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace,qea,t);
+		System.err.println("Running with "
+				+ MonitorFactory.create(qea).getClass());
+		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
+				.makeThree();
+		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
 		Verdict v = m.monitor();
-		System.err.println("Verdict for three was "+v);
+		System.err.println("Verdict for three was " + v);
 	}
-	public static void runFour() throws IOException, ParserConfigurationException, SAXException{				
+
+	public static void runFour() throws IOException,
+			ParserConfigurationException, SAXException {
 		QEA qea = (new properties.competition.Stepr()).makeFour();
-		System.err.println("Running with "+MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators()).makeFour();
-		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace,qea,t);
+		System.err.println("Running with "
+				+ MonitorFactory.create(qea).getClass());
+		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
+				.makeFour();
+		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
 		Verdict v = m.monitor();
-		System.err.println("Verdict for four was "+v);
-	}	
-	
+		System.err.println("Verdict for four was " + v);
+	}
+
 }
