b1bf4fb006458aa15b12e50b1e8d31caea9d787c
hcuenca <helena.cuenca@gmail.com>
2014-07-19 14:59:52 +0100

    *Method getMonitorClass created in FileMonitor *Number of events are now printed in XMLFileMonitorSAX *STePr benchmarks modified to capture XMLFailureException

--- code/qea/src/benchmark/competition/offline/Stepr.java
+++ code/qea/src/benchmark/competition/offline/Stepr.java
@@ -4,73 +4,141 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import monitoring.impl.MonitorFactory;
 import monitoring.impl.XMLFileMonitorSAX;
 import monitoring.impl.translators.OfflineTranslator;
 
 import org.xml.sax.SAXException;
 
+import properties.competition.translators.SteprTranslators;
 import structure.impl.other.Verdict;
 import structure.intf.QEA;
+import exceptions.XMLFailureException;
 
 public class Stepr {
+
+	static properties.competition.Stepr s = new properties.competition.Stepr();
+	static SteprTranslators tm = new SteprTranslators();
 
 	public static void main(String[] args) throws IOException,
 			ParserConfigurationException, SAXException {
 		// runOne();
 		// runTwo();
-		runThree();
-		// runFour();
+		// runThree();
+		runFour();
 	}
 
 	static String trace = "traces/Team6/log.xml";
 
 	public static void runOne() throws IOException,
 			ParserConfigurationException, SAXException {
-		QEA qea = (new properties.competition.Stepr()).makeOne();
-		System.err.println("Running with "
-				+ MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
-				.makeOne();
+
+		long startTime = System.currentTimeMillis();
+		QEA qea = s.makeOne();
+		OfflineTranslator t = tm.makeOne();
 		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
-		Verdict v = m.monitor();
-		System.err.println("Verdict for one was " + v);
+		System.err.println("Running with " + m.getMonitorClass());
+		long beforeMonitoring = System.currentTimeMillis();
+
+		try {
+			Verdict v = m.monitor();
+			System.err.println("STePr 1: Verdict was " + v);
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		} catch (XMLFailureException e) {
+			System.err.println("STePr 1: " + e.getMessage());
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		}
+
 	}
 
 	public static void runTwo() throws IOException,
 			ParserConfigurationException, SAXException {
-		QEA qea = (new properties.competition.Stepr()).makeTwo();
-		System.err.println("Running with "
-				+ MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
-				.makeTwo();
+
+		long startTime = System.currentTimeMillis();
+		QEA qea = s.makeTwo();
+		OfflineTranslator t = tm.makeTwo();
 		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
-		Verdict v = m.monitor();
-		System.err.println("Verdict for two was " + v);
+		System.err.println("Running with " + m.getMonitorClass());
+		long beforeMonitoring = System.currentTimeMillis();
+
+		try {
+			Verdict v = m.monitor();
+			System.err.println("STePr 2: Verdict was " + v);
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		} catch (XMLFailureException e) {
+			System.err.println("STePr 2: " + e.getMessage());
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		}
 	}
 
 	public static void runThree() throws IOException,
 			ParserConfigurationException, SAXException {
-		QEA qea = (new properties.competition.Stepr()).makeThree();
-		System.err.println("Running with "
-				+ MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
-				.makeThree();
+
+		long startTime = System.currentTimeMillis();
+		QEA qea = s.makeThree();
+		OfflineTranslator t = tm.makeThree();
 		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
-		Verdict v = m.monitor();
-		System.err.println("Verdict for three was " + v);
+		System.err.println("Running with " + m.getMonitorClass());
+		long beforeMonitoring = System.currentTimeMillis();
+
+		try {
+			Verdict v = m.monitor();
+			System.err.println("STePr 3: Verdict was " + v);
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		} catch (XMLFailureException e) {
+			System.err.println("STePr 3: " + e.getMessage());
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		}
 	}
 
 	public static void runFour() throws IOException,
 			ParserConfigurationException, SAXException {
-		QEA qea = (new properties.competition.Stepr()).makeFour();
-		System.err.println("Running with "
-				+ MonitorFactory.create(qea).getClass());
-		OfflineTranslator t = (new properties.competition.translators.SteprTranslators())
-				.makeFour();
-		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
-		Verdict v = m.monitor();
-		System.err.println("Verdict for four was " + v);
-	}
 
+		long startTime = System.currentTimeMillis();
+		QEA qea = s.makeFour();
+		OfflineTranslator t = tm.makeFour();
+		XMLFileMonitorSAX m = new XMLFileMonitorSAX(trace, qea, t);
+		System.err.println("Running with " + m.getMonitorClass());
+		long beforeMonitoring = System.currentTimeMillis();
+
+		try {
+			Verdict v = m.monitor();
+			System.err.println("STePr 4: Verdict was " + v);
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		} catch (XMLFailureException e) {
+			System.err.println("STePr 4: " + e.getMessage());
+			long endTime = System.currentTimeMillis();
+			System.err.println(">>Execution time without creation: "
+					+ (endTime - beforeMonitoring));
+			System.err.println(">>Total execution time : "
+					+ (endTime - startTime));
+		}
+	}
 }
